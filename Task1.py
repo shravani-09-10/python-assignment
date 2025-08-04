a4842f40bb82f34230699b0bbe190d2884b5f8b0
shravani-09-10 <shravanishivapur@gmail.com>
2025-08-04 20:47:41 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+#python program using basic arithmetic operations
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+
+add=a+b
+print("addition of two numbers: " +str(add))
+
+prod=a*b
+print("multiplication of two numbers:" + str(prod))
+
+div=a/b
+print("divison of two numbers:" + str(div))
+
+sub=a-b
+print("substraction of two numbers:" + str(sub))
