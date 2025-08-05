b8933457654d776ff3d836d0bc80d448c9d18274
shravani-09-10 <shravanishivapur@gmail.com>
2025-08-05 20:56:08 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,15 +1,5 @@
-#python program using basic arithmetic operations
-a=int(input("enter a number:"))
-b=int(input("enter a number:"))
-
-add=a+b
-print("addition of two numbers: " +str(add))
-
-prod=a*b
-print("multiplication of two numbers:" + str(prod))
-
-div=a/b
-print("divison of two numbers:" + str(div))
-
-sub=a-b
-print("substraction of two numbers:" + str(sub))
+n=int(input("enter a number:"))
+if n%2==0:
+    print(n," is even number")
+else:
+    print(n,"is odd number")
--- Task2.py
+++ Task2.py
@@ -1,5 +1,4 @@
-first_name = input("enter your first name: ")
-last_name = input("enter your second name: ")
-
-full_name = first_name+" "+last_name
-print("hello," + full_name +"! welcome to pyhton program")
+total=0
+for i in range (0,51):
+    total += i
+print("sum of integers from 1 to 50",total)
